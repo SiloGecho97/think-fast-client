43d6f7d2d56f6805e96bd97ecbd0ec9c9bc0a746
Silas Getachew <silogecho@gmail.com>
2022-02-22 21:15:01 +0300

    fix: add react-hooks eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,10 @@ module.exports = {
   },
   plugins: [
     'react',
+    "react-hooks"
   ],
   rules: {
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
   },
 };
