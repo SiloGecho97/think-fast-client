16f5ef9230850d0185e426e8454ee5d5f103ac57
Silas Getachew <silogecho@gmail.com>
2022-02-26 08:11:50 +0300

    fix: landing header and home

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import Header from '../partials/Header'
 import HeroHome from '../partials/HeroHome'
 import FeaturesHome from '../partials/Features'
 import FeaturesBlocks from '../partials/FeaturesBlocks'
-import Testimonials from '../partials/Testimonials'
 import Newsletter from '../partials/Newsletter'
 import Footer from '../partials/Footer'
+import LandingHeader from '../partials/LandingHeader'
 const Index = () => {
   return (
     <>
+     <LandingHeader/>
       <HeroHome />
       <FeaturesHome />
       <FeaturesBlocks />
-      <Testimonials />
-      <Newsletter />
       <Footer />
      
     </>
--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,28 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
 
-function Header() {
-
-  const [top, setTop] = useState(true);
-
-  // detect whether user has scrolled the page down by 10px 
-  useEffect(() => {
-    const scrollHandler = () => {
-      window.pageYOffset > 10 ? setTop(false) : setTop(true)
-    };
-    window.addEventListener('scroll', scrollHandler);
-    return () => window.removeEventListener('scroll', scrollHandler);
-  }, [top]);  
-
+const Header = () => {
   return (
-    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white backdrop-blur-sm shadow-lg'}`}>
-      <div className="max-w-6xl mx-auto px-5 sm:px-6">
-        <div className="flex items-center justify-between h-16 md:h-20">
-
-          {/* Site branding */}
-          <div className="flex-shrink-0 mr-4">
-            {/* Logo */}
-            <Link to="/" className="block" aria-label="Cruip">
+    <div>
+      <nav className="bg-white px-2 sm:px-4 py-4 rounded border-b border-gray-50 shadow">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
+          <a href="/" className="flex">
             <svg
               className="mr-3 h-10"
               viewBox="0 0 52 72"
@@ -42,31 +25,111 @@ function Header() {
                 fill="#1C64F2"
               />
             </svg>
-            </Link>
+            <span className="self-center text-xl font-bold whitespace-nowrap text-primary">
+              ThinkFast
+            </span>
+          </a>
+          <div className="flex items-center md:order-2">
+            <button
+              type="button"
+              className="flex mr-3 text-sm bg-gray-100 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 "
+              id="user-menu-button"
+              aria-expanded="false"
+              data-dropdown-toggle="dropdown"
+            >
+              <span className="sr-only">Open user menu</span>
+              <img
+                className="w-8 h-8 rounded-full"
+                src="https://image.shutterstock.com/image-vector/default-avatar-profile-trendy-style-260nw-1759726295.jpg"
+                alt="user "
+              />
+            </button>
+
+            <div
+              className="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow"
+              id="dropdown"
+            >
+              <div className="py-3 px-4">
+                <span className="block text-sm text-gray-900 dark:text-white">
+                  Bonnie Green
+                </span>
+                <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">
+                  name@flowbite.com
+                </span>
+              </div>
+              <ul className="py-1" aria-labelledby="dropdown">
+                <li>
+                  <a
+                    href="/"
+                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-500 "
+                  >
+                    Dashboard
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-500"
+                  >
+                    Settings
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-500"
+                  >
+                    Earnings
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-500"
+                  >
+                    Sign out
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <button
+              data-collapse-toggle="mobile-menu-2"
+              type="button"
+              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
+              aria-controls="mobile-menu-2"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <svg
+                className="hidden w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
           </div>
-
-          {/* Site navigation */}
-          <nav className="flex flex-grow">
-            <ul className="flex flex-grow justify-end flex-wrap items-center">
-              <li>
-                <Link to="/login" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out dark:bg-gray-100">Sign in</Link>
-              </li>
-              <li>
-                <Link to="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
-                  <span>Sign up</span>
-                  <svg className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
-                  </svg>                  
-                </Link>
-              </li>
-            </ul>
-
-          </nav>
-
         </div>
-      </div>
-    </header>
-  );
+      </nav>
+    </div>
+  )
 }
 
-export default Header;
+export default Header
