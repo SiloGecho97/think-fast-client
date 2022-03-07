3e6d0aabfe149860b305a2c78376b14b89d33ec1
Silas Getachew <silogecho@gmail.com>
2022-03-07 22:57:58 +0300

    start simple redux-saga

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+
+// Reducers
+import todo from './todo-reducer'
+
+export default combineReducers({
+  todo,
+  // Here you can registering another reducers.
+})
