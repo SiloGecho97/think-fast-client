4f0819a2e2b8a42dd1e511593433c748eaf90fe5
Silas Getachew <silogecho@gmail.com>
2022-03-18 12:14:19 +0300

    Fix navbar dropdown

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Header = () => {
+  const navigate = useNavigate()
+  const logout = () => {
+    localStorage.removeItem('user')
+    localStorage.removeItem('info')
+    navigate('/')
+  }
   return (
     <div>
       <nav className="bg-white px-2 sm:px-4 py-4 rounded border-b border-gray-50 shadow">
@@ -30,101 +37,30 @@ const Header = () => {
             </span>
           </a>
           <div className="flex items-center md:order-2">
-            <button
-              type="button"
-              className="flex mr-3 text-sm bg-gray-100 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 "
-              id="user-menu-button"
-              aria-expanded="false"
-              data-dropdown-toggle="dropdown"
-            >
-              <span className="sr-only">Open user menu</span>
-              <img
-                className="w-8 h-8 rounded-full"
-                src="https://image.shutterstock.com/image-vector/default-avatar-profile-trendy-style-260nw-1759726295.jpg"
-                alt="user "
-              />
-            </button>
-
-            <div
-              className="hidden z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow"
-              id="dropdown"
-            >
-              <div className="py-3 px-4">
-                <span className="block text-sm text-gray-900 dark:text-white">
-                  Bonnie Green
-                </span>
-                <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">
-                  name@flowbite.com
-                </span>
+          <div className="dropdown dropdown-end">
+            <div tabindex="0" className="avatar online cursor-pointer">
+              <div className="w-8 rounded-full">
+                <img
+                  src="https://api.lorem.space/image/face?hash=28212"
+                  alt="Avatar"
+                />
               </div>
-              <ul className="py-1" aria-labelledby="dropdown">
-                <li>
-                  <a
-                    href="/"
-                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-500 "
-                  >
-                    Dashboard
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/"
-                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-500"
-                  >
-                    Settings
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/"
-                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-500"
-                  >
-                    Earnings
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="/"
-                    className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-500"
-                  >
-                    Sign out
-                  </a>
-                </li>
-              </ul>
             </div>
-            <button
-              data-collapse-toggle="mobile-menu-2"
-              type="button"
-              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
-              aria-controls="mobile-menu-2"
-              aria-expanded="false"
+            <ul
+              tabindex="0"
+              className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52"
             >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <svg
-                className="hidden w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </button>
+              <li>
+                <a href="/#">Account Setting</a>
+              </li>
+              <li>
+                <Link to="/withdraw">Withdraw</Link>
+              </li>
+              <li onClick={(e) => logout()}>
+                <a href="/#">Logout</a>
+              </li>
+            </ul>
+          </div>
           </div>
         </div>
       </nav>
