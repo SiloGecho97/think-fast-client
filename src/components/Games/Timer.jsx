27a9c443a4ff3e39efc8f9c7df26e8f13ac20843
Silas Getachew <silogecho@gmail.com>
2022-04-07 18:57:39 +0300

    Fix PWA issue

--- src/components/Games/Timer.jsx
+++ src/components/Games/Timer.jsx
@@ -23,7 +23,7 @@ const renderer = ({ hours, minutes, seconds, completed }) => {
 const Timer = () => {
   return (
     <div className="shadow-md w-max my-4 p-4 rounded-xl border shadow-gray-200 border-gray-200">
-      <Countdown date={Date.now() + 30000} renderer={renderer} />
+      <Countdown date={Date.now() + 10000} renderer={renderer} />
     </div>
   )
 }
