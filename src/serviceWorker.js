ce6932cb3d6bce2cfab2adf228f43d41388c3df3
Silas Getachew <silogecho@gmail.com>
2022-04-09 15:33:47 +0300

    Add installation prompt

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -137,5 +137,21 @@
   //   }
   // }
 
+  let deferredPrompt
+
+  window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault()
+    deferredPrompt = e
+  })
+
+  window.addEventListener('appinstalled', () => {
+    // Hide the app-provided install promotion
+    // hideInstallPromotion()
+    // Clear the deferredPrompt so it can be garbage collected
+    deferredPrompt = null
+    // Optionally, send analytics event to indicate successful install
+    console.log('PWA was installed')
+  })
+
   register()
 })
